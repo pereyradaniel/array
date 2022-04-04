3973ff12b11441ada989fd6d35d1939f1f2d4833
Daniel Pereyra <pereyradaniell@gmail.com>
2022-04-04 17:11:49 -0300

    cambio de directorio

--- main.js
+++ main.js
@@ -1,87 +1,102 @@
 
-let registro = true;
+const encuestados = [];
+const candidatos = [];
 let nombre;
 let edad;
-let votos1 = 0;
-let votos2 = 0;
-let total = 0;
+let encuestado;
 let candidato;
+let indice;
 
-// funcion cuenta votos
-function totalvotos(){
-  if (candidato == "AXEL"){
-      votos1 = votos1+1;
-      alert ("Total de respuestas de la provincia de Buenos Aires "+votos1);
-
-  } else if (candidato =="RODOLFO"){
-      votos2 = votos2+1;
-      alert ("Total de respuestas de la provincia de Mendoza "+votos2);
+// class encuestado
+class Encuestado {
+  constructor(nombre, edad, provincia) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.provincia = provincia;
+  }
+  saludar() {
+    alert(`Hola ${this.nombre}, Vamos a comenzar con una breve encuesta`);
   }
 }
 
-// funcion total votantes
-function totalvotantes(){
-  total = votos1 + votos2;
-  alert("Total de respuestas de la encuesta "+total);
+// class candidato
+class Candidato {
+  constructor(nombre, provincia) {
+    this.nombre = nombre;
+    this.provincia = provincia;
+    this.votos = 0;
+  }
 }
 
+// registro de candidatos en la clase
+candidatos.push(new Candidato("Axel", "BUENOS AIRES"));
+candidatos.push(new Candidato("Rodolfo", "MENDOZA"));
 
+// console.log(candidatos);
 
-class Persona {
-  constructor(nombre, edad){
-      this.nombre = nombre;
-      this.edad = edad;
+function registroEncuesta(){
+do {
+  nombre = prompt("Ingrese su Nombre");
+  edad = prompt("Ingrese su Edad");
+
+  if (nombre != "" && edad >= 18) {
+    provincia = prompt("Ingrese su Provincia").toUpperCase();
+    encuestado = new Encuestado(nombre, edad, provincia);
+    let provinciaEncuestado = candidatos.find((p) => p.provincia == provincia);
+    if (provinciaEncuestado) {
+      encuestados.push(encuestado);
+      let indice = candidatos.findIndex((c) => c.provincia == provincia);
+      candidatos[indice].votos = candidatos[indice].votos + 1;
+      alert(
+        "Cantidad de participantes de tu misma provincia " +
+          candidatos[indice].votos
+      );
+    }else{
+        alert(nombre+" No te encuentras dentro de las pronvincias registradas. Gracias");
+        break;
+    }
+    encuestado.saludar();
+    entrada = prompt(
+      " Ingrese su provincia: \n1. para Buenos Aires \n2. para Mendoza \n3. 3 para salir"
+    );
+    switch (entrada) {
+      case "1":
+        candidato = prompt(
+          "Ingrese en nombre del Gobernador de Buenos Aires"
+        ).toUpperCase();
+        if (candidato == "AXEL") {
+          alert("Tu respuesta es correcta");
+        } else {
+          alert("Fallaste en la respuesta");
+        }
+        break;
+
+      case "2":
+        candidato = prompt(
+          "Ingrese en nombre del Gobernador de Mendoza"
+        ).toUpperCase();
+        if (candidato == "RODOLFO") {
+          alert("Tu respuesta es correcta");
+        } else {
+          alert("Fallaste en la respuesta");
+        }
+        break;
+        default:
+        break;
+    }
+  } else if (edad < 18) {
+    alert("La encuesta es para mayores de 18 años, Gracias");
+  } else if (nombre == "") {
+    alert("Tiene que completar su nombre");
+    registroEncuesta();
   }
-  saludar(){
-            alert(`Hola ${this.nombre}, Vamos a comenzar con una breve encuesta`);
+} while (entrada != "3");
+{
+  alert(
+    "Cantidad de participantes de tu misma provincia " +
+      candidatos[indice].votos
+  );
+  alert("Hasta la Proxima " + nombre);
 }}
 
-
-while(registro == true){
-   nombre = prompt("Ingrese un nombre");
-   edad = prompt("Ingrese una edad");
-  let personas = new Persona(nombre, edad);
-
-  if((nombre!="") && (edad>=18)){
-    personas.saludar();
-    entrada = prompt(" Ingrese: \n1. para Buenos Aires \n2. para Mendoza \n3. 3 para salir");
-    while(entrada!="3"){
-      switch(entrada){
-        case "1":
-          candidato = prompt("Ingrese en nombre del Gobernador de Buenos Aires").toUpperCase();
-                 if( candidato == "AXEL"){
-                     alert("Tu respuesta es correcta");
-
-                     totalvotos();
-
-                 }else {
-                     alert("Fallaste en la respuesta");
-                 }
-                 break;
-                 case '2':
-                  candidato = prompt("Ingrese en nombre del Gobernador de Mendoza").toUpperCase();
-                 if( candidato == "RODOLFO"){
-                     alert("Tu respuesta es correcta");
-                     totalvotos();
-
-                 }else {
-                     alert("Fallaste en la respuesta");
-                 }
-                  break;
-              default:
-                  alert("Ingrese otro dato");
-                  break;
-      }
-      entrada = prompt(" Ingrese: \n1. para Buenos Aires \n2. para Mendoza \n3. 3 para salir");
-      if (entrada =="3"){
-          alert(nombre+" gracias por participar");
-          totalvotantes();
-      }
-    }
-
-  }else if(edad<18){
-    alert("La encuesta es para mayores de 18 años, Gracias");
-  }else if(nombre ==""){
-    alert("Tiene que completar su nombre");
-  }
-}
+registroEncuesta();
